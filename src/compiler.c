10a59fd19b0215008036fb292845ac2ff8dcdb1c
finlaywashere <finman292004@protonmail.com>
2021-04-08 10:03:27 -0400

    Added really broken parsing stuff

--- src/compiler.c
+++ src/compiler.c
@@ -90,19 +90,86 @@ void create_elf(uint8_t* buffer, uint8_t* data, uint64_t data_length){
 	free(programs);
 	free(segments);
 }
+uint64_t get_instruction_length(char* str){
+	//TODO: Find instruction length for string
+	if(!memcmp(str,"mov",3)){
+		return 5;
+	}
+	if(!memcmp(str,"ret",3)){
+		return 1;
+	}
+	return 0;
+}
+void parse_and_write_instruction(char* str, uint8_t* buffer){
+	//TODO: Support more registers
+	if(!memcmp(str,"ret",3)){
+		buffer[0] = 0xC3;
+	}
+	if(!memcmp(str,"mov",3)){
+		uint64_t reg = 0;
+		uint64_t val = 0;
+		
+		uint64_t count = 0;
+		uint64_t index = 0;
+		for(uint64_t i = 3; i < strlen(str); i++){
+			if(str[i] == ',' || i == strlen(str)-1){
+				char* str2 = (char*)(((uint64_t)str)+index);
+				if(count == 0){
+					if(!memcmp(str2,"eax",3)){
+						reg = 0;
+					}
+				}else if(count == 1){
+					val = strtol(str2,NULL,10);
+				}
+				index = i + 1;
+				count++;
+			}
+		}
+		buffer[0] = 0xb8 + reg;
+		buffer[1] = (uint8_t) val;
+		buffer[2] = (uint8_t) (val >> 8);
+		buffer[3] = (uint8_t) (val >> 16);
+		buffer[4] = (uint8_t) (val >> 24);
+	}
+	return;
+}
 
 int main(){
-	FILE* bin = fopen("test.bin", "r");
-	fseek(bin, 0L, SEEK_END);
-	uint64_t len = ftell(bin);
-	rewind(bin);
-	uint8_t* buffer = malloc(len); // Allocate bytes for data
-	fread(buffer,len,1,bin);
-	fclose(bin);
+	FILE* src = fopen("asm/test_intel.asm", "r");
+	fseek(src, 0L, SEEK_END);
+	uint64_t len = ftell(src);
+	rewind(src);
+	char* buffer = malloc(len); // Allocate bytes for data
+	fread(buffer,len,1,src);
+	fclose(src);
+	
+	uint64_t bin_len = 0;
+	uint64_t index = 0;
+	for(uint64_t i = 0; i < len; i++){
+		if(buffer[i] == '\n' || i == len - 1){
+			uint64_t tmp_len = i - index;
+			char* str = malloc(tmp_len);
+			memcpy(str,(char*)(((uint64_t)buffer)+index),tmp_len);
+			uint64_t inst_len = get_instruction_length(str);
+			bin_len += inst_len;
+			index = i+1;
+		}
+	}
+	index = 0;
+	uint8_t* bin_buffer = malloc(bin_len);
+	for(uint64_t i = 0; i < len; i++){
+                if(buffer[i] == '\n' || i == len - 1){
+                        uint64_t tmp_len = i - index;
+                        char* str = malloc(tmp_len);
+                        memcpy(str,(char*)(((uint64_t)buffer)+index),tmp_len);
+                        parse_and_write_instruction(str,(uint8_t*)(((uint64_t)bin_buffer)+index));
+			index = i+1;
+                }
+        }
 	
 	uint64_t elen = elf_length(len);
 	uint8_t* elf_buffer = malloc(elen);
-	create_elf(elf_buffer,buffer,len);
+	create_elf(elf_buffer,bin_buffer,len);
 
 	FILE* fd = fopen("test.elf","w+");
 	fwrite(elf_buffer,elen,1,fd);
