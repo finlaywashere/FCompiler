1fa0cfa89ecb72a861fbd8f54f8409e812b75dd5
finlaywashere <finman292004@protonmail.com>
2021-04-08 09:42:04 -0400

    Got objdump working with new generation system

--- src/compiler.c
+++ src/compiler.c
@@ -3,10 +3,10 @@
 #include <stdio.h>
 #include <string.h>
 
-#define SEGMENTS 2
+#define SEGMENTS 3
 #define PROGRAMS 1
 
-const char* names = ".text\0.shstrtab\0";
+const char* names = "\0.text\0.shstrtab\0";
 
 void make_valid_header(elf_header_t* header){
 	//NOTE: This doesn't set any fields that should be filled with zeros, and doesn't set any non static values
@@ -39,7 +39,7 @@ uint64_t elf_length(uint64_t data_length){
 }
 void create_elf(uint8_t* buffer, uint8_t* data, uint64_t data_length){
 	elf_header_t *header = (elf_header_t*) malloc(sizeof(elf_header_t));
-    make_valid_header(header);
+	make_valid_header(header);
 	
 	uint64_t file_offset = elf_length(data_length)-data_length;
 	uint64_t str_len = file_offset - (0x40 + 0x40 * SEGMENTS + 0x38 * PROGRAMS);
@@ -49,36 +49,36 @@ void create_elf(uint8_t* buffer, uint8_t* data, uint64_t data_length){
 	header->segment_header = 0x40 + 0x38 * PROGRAMS; // Right after program headers
 	header->num_segment_headers = SEGMENTS;
 	header->entry_point = file_offset;
-	header->segment_header_name_index = 1;
+	header->segment_header_name_index = 2;
 
 	program_header_t* programs = (program_header_t*) malloc(sizeof(program_header_t)*PROGRAMS);
-	
+	memset(programs,0,sizeof(program_header_t)*PROGRAMS);
 	segment_header_t* segments = (segment_header_t*) malloc(sizeof(segment_header_t)*SEGMENTS);
-	
+	memset(segments,0,sizeof(segment_header_t)*SEGMENTS);
 	uint64_t name_index = 0;
 	for(uint64_t i = 0; i < SEGMENTS; i++){
 		segments[i].name_offset = name_index;
 		name_index += strlen((char*) (((uint64_t) names) + name_index)) + 1;
 	}
 
-    programs[0].type = PROGRAM_LOAD;
-    programs[0].offset = file_offset;
-    programs[0].virtual_address = file_offset;
-    programs[0].file_size = data_length;
-    programs[0].memory_size = data_length;
-    programs[0].flags = 0x5;
-    programs[0].align = 0x1000;
+	programs[0].type = PROGRAM_LOAD;
+	programs[0].offset = file_offset;
+	programs[0].virtual_address = file_offset;
+	programs[0].file_size = data_length;
+	programs[0].memory_size = data_length;
+	programs[0].flags = 0x5;
+	programs[0].align = 0x1000;
 	
-	segments[0].type = SEGMENT_PROGRAM;
-	segments[0].flags = SEGMENT_FLAG_EXEC | SEGMENT_FLAG_ALLOC;
-	segments[0].mem_address = file_offset;
-	segments[0].file_offset = file_offset;
-	segments[0].file_size = data_length;
+	segments[1].type = SEGMENT_PROGRAM;
+	segments[1].flags = SEGMENT_FLAG_EXEC | SEGMENT_FLAG_ALLOC;
+	segments[1].mem_address = file_offset;
+	segments[1].file_offset = file_offset;
+	segments[1].file_size = data_length;
 
-	segments[1].type = SEGMENT_STRINGS;
-	segments[1].flags = SEGMENT_FLAG_STRINGS;
-	segments[1].file_offset = file_offset-str_len;
-	segments[1].file_size = str_len;
+	segments[2].type = SEGMENT_STRINGS;
+	segments[2].flags = SEGMENT_FLAG_STRINGS;
+	segments[2].file_offset = file_offset-str_len;
+	segments[2].file_size = str_len;
 	
 	memcpy(buffer,header,sizeof(elf_header_t));
 	memcpy((uint8_t*) (((uint64_t)buffer)+0x40),programs,sizeof(program_header_t)*PROGRAMS);
